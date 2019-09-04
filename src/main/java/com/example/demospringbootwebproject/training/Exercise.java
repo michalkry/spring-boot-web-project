a2e395c3b549ec6729303f5364485a92d715891c
Michal Kryszczak <kryszczakmichal1@gmail.com>
2019-09-04 17:40:38 +0200

    exercise view in progress

--- src/main/java/com/example/demospringbootwebproject/training/Exercise.java
+++ src/main/java/com/example/demospringbootwebproject/training/Exercise.java
@@ -2,33 +2,33 @@ package com.example.demospringbootwebproject.training;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
-
+@Setter
+@Getter
+@NoArgsConstructor
 @AllArgsConstructor
 @Entity
 public class Exercise {
+
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    private String exercise;
+    private String name;
     private Integer sets;
     private Integer reps;
     private Integer tempo;
     private Integer rest;
 
-
-
-
     @ManyToOne
-    @JoinColumn (name = "trainingId")
+    @JoinColumn(name = "trainingId")
     private Training training;
 
-
-
-
-    }
+}
 
--- src/main/java/com/example/demospringbootwebproject/training/ExerciseController.java
+++ src/main/java/com/example/demospringbootwebproject/training/ExerciseController.java
@@ -4,7 +4,9 @@ package com.example.demospringbootwebproject.training;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
@@ -25,7 +27,28 @@ public class ExerciseController {
         model.addAttribute("exercises", exercisesList);
 
         return "exercises";
+    }
+
+    @ModelAttribute("exercise")
+    public Exercise createModel() {
+        return new Exercise();
+    }
+
+    @RequestMapping(path = "/save", method = RequestMethod.POST)
+    public String save(@ModelAttribute Exercise exercise, Model model){
+
+        exerciseRepository.save(exercise);
+        List<Exercise> exercises = exerciseRepository.findAll();
+        model.addAttribute("exercises", exercises);
+
+        return "addExercise";
 
     }
+
+    @GetMapping("/add")
+    public String redirectToNewPage(){
+        return "addExercise";
+    }
+
 
 }
